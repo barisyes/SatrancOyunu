f050aa633f325c76310eed5e27acac2ab991fe81
barisyes <barisyesilkayaa@gmail.com>
2022-05-26 17:43:27 +0300

    Initial Commit

---
+++ SatrancOyunu/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SatrancOyunu
+//
+//  Created by Barış Yeşilkaya on 26.05.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
